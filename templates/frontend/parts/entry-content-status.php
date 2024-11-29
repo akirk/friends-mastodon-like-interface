58e2fe6cba2e0176f0ebd97c7c1d5c8367a7792d
Alex Kirk <akirk@users.noreply.github.com>
2024-11-29 09:21:22 +0100

    Allow expanding of overly long entries

--- templates/frontend/parts/entry-content-status.php
+++ templates/frontend/parts/entry-content-status.php
@@ -13,7 +13,32 @@ $content = get_the_content();
 	if ( empty( $content ) ) {
 		the_title();
 	} else {
-		the_content();
+		$summary_cutoff = apply_filters( 'friends_mastodon_like_summary_cutoff', 600 );
+		if ( strlen( $content ) > $summary_cutoff ) {
+			$excerpt = substr( $content, 0, $summary_cutoff );
+			$excerpt = substr( $excerpt, 0, strrpos( $excerpt, ' ' ) );
+			// close tags that have not been ended with a >
+			$excerpt = preg_replace( '/<([a-z]+)([^>]*[^\/])$/', '<$1$2>', $excerpt );
+			$excerpt = force_balance_tags( $excerpt );
+			$excerpt = apply_filters( 'the_content', $excerpt );
+
+
+
+
+			?><details>
+				<summary>
+				<?php
+				echo wp_kses_post( $excerpt );
+				?>
+				</summary>
+				<?php
+				the_content();
+				?>
+			</details>
+			<?php
+		} else {
+			the_content();
+		}
 	}
 	?>
 </div>
